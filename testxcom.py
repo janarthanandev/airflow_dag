4d0a39498427cc8c1585ed259a7e3abb2931dbe4
Janarthanan <janarthanandevs@gmail.com>
2021-04-22 19:47:13 +0530

    Update testxcom.py

--- testxcom.py
+++ testxcom.py
@@ -9,8 +9,8 @@ DAG = DAG(
 )
 
 def push_function(**kwargs):
-    ls = ['a', 'b', 'c']
-    return ls
+    result = {'ortho': 's3://pwlab-dataset/output/2021-04-22 17:00:48/orthophoto.tif', 'photogrametry_result': 's3://pwlab-dataset/output/2021-04-22 17:00:48/all.zip'}
+    return result
 
 push_task = PythonOperator(
     task_id='push_task', 
@@ -22,6 +22,7 @@ def pull_function(**kwargs):
     ti = kwargs['ti']
     ls = ti.xcom_pull(task_ids='push_task')
     print(ls)
+    print("ortho",ls.get('ortho'))
 
 pull_task = PythonOperator(
     task_id='pull_task', 
