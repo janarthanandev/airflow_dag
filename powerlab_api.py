427720e0f19711de57d8b03c26f8c8f159a49ae0
Janarthanan <janarthanandevs@gmail.com>
2021-04-27 11:21:15 +0530

    Update powerlab_api.py

--- powerlab_api.py
+++ powerlab_api.py
@@ -29,7 +29,7 @@ dag = DAG(
     tags=['pwlab']
 )
 
-API_URL="http://18.212.147.213:32060"
+API_URL="http://54.81.159.231:32060"
 
 def blur_detection(ds, **kwargs):
     dqurl = API_URL+"/dq/blur"
